dd41c76e2fdd92d89d856ffd362947507f489ad4
cfico-vi <cfico-vi@student.42sp.org.br>
2021-02-11 17:20:04 -0300

    make funcionando e função

--- Libft/ft_memset.c
+++ Libft/ft_memset.c
@@ -6,12 +6,12 @@
 /*   By: cfico-vi <cfico-vi@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/10 17:35:58 by cfico-vi          #+#    #+#             */
-/*   Updated: 2021/02/11 13:13:06 by cfico-vi         ###   ########.fr       */
+/*   Updated: 2021/02/11 16:42:23 by cfico-vi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
 #include <stdio.h>
+#include "libft.h"
 
 void	*ft_memset(void *str, int c, size_t len)
 {
@@ -20,15 +20,8 @@ void	*ft_memset(void *str, int c, size_t len)
 
 	ptr = str;
 	i = 0;
+
 	while (i < len)
 		*(ptr + i++) = c;
 	return (str);
-}
-
-int main ()
-{
-	char str[50];
-	strcpy (str, "This string");
-	ft_memset(str, 'B', 4);
-	puts(str);
 }
