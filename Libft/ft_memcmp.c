4d85d944baa8224065412f2d106d1e02f7a66e96
cfico-vi <cfico-vi@student.42sp.org.br>
2021-02-17 10:31:29 -0300

    ft_memcmp pra testar

--- Libft/ft_memcmp.c
+++ Libft/ft_memcmp.c
@@ -12,18 +12,18 @@
 
 #include "libft.h"
 
-int	memcmp(const void *s1, const void *s2, size_t n)
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
 	size_t			i;
-	unsigned char	*ptr;
+	unsigned char	*ptr1;
 	unsigned char	*ptr2;
 
-	i = 0;
-	ptr = (unsigned char *)s1;
+	i = -1;
+	ptr1 = (unsigned char *)s1;
 	ptr2 = (unsigned char *)s2;
-	while (i < n && *(ptr + i) == *(ptr2 + i))
-		i++;
-	if (n)
-		return (*(ptr + i) - *(ptr2 + i));
-	return (0);
+	while (++i < n && *(ptr1 + i) == *(ptr2 + i))
+		;
+	if (i == n)
+		return (0);
+	return (*(ptr1 + i) - *(ptr2 + i));
 }
--- Libft/libft.h
+++ Libft/libft.h
@@ -25,7 +25,7 @@ int		ft_isprint(int c);
 int		ft_isspace(int c);
 void	*ft_memccpy(void *dest, const void *src, int c, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
-int		memcmp(const void *s1, const void *s2, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memset(void *str, int c, size_t len);
