d2d64795d227c8b68ab4f887ff0c891d025fe0cd
cfico-vi <cfico-vi@student.42sp.org.br>
2021-02-18 19:56:31 -0300

    substr sem comentário

--- Libft/ft_substr.c
+++ Libft/ft_substr.c
@@ -6,16 +6,11 @@
 /*   By: cfico-vi <cfico-vi@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/18 18:43:44 by cfico-vi          #+#    #+#             */
-/*   Updated: 2021/02/18 18:58:20 by cfico-vi         ###   ########.fr       */
+/*   Updated: 2021/02/18 19:55:57 by cfico-vi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-/*
-**Allocates (with malloc(3)) and returns a substringfrom the string ’s’.
-**The substring begins at index ’start’ and is of maximum size ’len’.
-*/
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
