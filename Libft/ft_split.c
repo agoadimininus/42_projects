022b6fb216b363bd9061028d63207dbb39f1f5b4
cfico-vi <cfico-vi@student.42sp.org.br>
2021-02-21 15:59:56 -0300

    split corrigido

--- Libft/ft_split.c
+++ Libft/ft_split.c
@@ -11,7 +11,6 @@
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
 size_t		ft_subcount(char *s, char c)
 {
@@ -55,17 +54,17 @@ char		**ft_split(char const *s, char c)
 	size_t	i;
 	size_t	j;
 
-	sub_count = 0;
-	if (!(sub_split = (char **)calloc((sub_count + 1), sizeof(char *))) || !s)
+	sub_count = ft_subcount((char *)s, c);
+	if (!(sub_split = (char **)malloc((sub_count + 1) * sizeof(char *))) || !s)
 		return (NULL);
 	sub_cpy = sub_split;
 	i = 0;
 	j = 0;
 	while (*(s + j))
 	{
-		if (*s != c)
+		if (*(s + j) != c)
 		{
-			while (*(s + i) != c && *(s + i) != '\0')
+			while (*(s + j + i) != c && *(s + j + i) != '\0')
 				i++;
 			*sub_cpy++ = ft_substr(s, j, i);
 			j += i - 1;
@@ -75,57 +74,4 @@ char		**ft_split(char const *s, char c)
 	}
 	sub_cpy = NULL;
 	return (sub_split);
-}
-
-static void            ft_print_tabstr(char **tabstr)
-{
-    printf("----------------------------------------------\n");
-    int        i;
-
-    i = 0;
-    while (tabstr[i] != NULL)
-    {
-        printf("          -%s-\n",tabstr[i]);
-        i++;
-    }
-    printf("----------------------------------------------\n");
-}
-
-
-static void            check_split(char *s, char c)
-{
-    char    **tabstr;
-
-    if (!(tabstr = ft_split(s, c)))
-        printf("NULL");
-    else
-        ft_print_tabstr(tabstr);
-}
-
-int                    main()
-{
-    int        arg;
-
-    arg = 1;
-    while (arg < 6 )
-    {
-        if (arg == 1)
-            check_split("          ", ' ');
-
-        else if (arg == 2)
-            check_split("2 lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse   ", ' ');
-
-        else if (arg == 3)
-            check_split("3       lorem      ipsum dolor     sit amet, consectetur   adipiscing elit. Sed non risus. Suspendisse   ", ' ');
-
-        else if (arg == 4)
-            check_split("4 lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultricies diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. i", 'i');
-
-        else
-            check_split("5 lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultricies diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. z", 'z');
-
-        arg++;
-    }
-    return (0);
-
 }
